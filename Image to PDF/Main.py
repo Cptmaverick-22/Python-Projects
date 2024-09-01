e5d3f2df426345fcd2beffe3829aa00b681c89eb
Cptmaverick-22 <sourasishain@gmail.com>
2024-09-01 22:05:48 +0530

    Added Image to Pdf

--- Image to PDF/Main.py
+++ Image to PDF/Main.py
@@ -1,12 +1,13 @@
-from fpdf import FPDF 
-from os import listdir 
+from fpdf import FPDF
+from PIL import Image
+from os import listdir, path as os_path
 
-path = "C:/Users/soura/Desktop/WIX"
+path = r"C:\Users\soura\Desktop\WIX"
 img_list = listdir(path)
 
 img_list.sort()
 
-pdf = FPDF("p","mm","A4")
+pdf = FPDF("p", "mm", "A4")
 
 x = 0
 y = 0
@@ -15,7 +16,9 @@ w = 210
 h = 297
 
 for img in img_list:
-    pdf.add_page()
-    pdf.image(path + img,x,y,w,h)
+    img_path = os_path.join(path, img)
+    if img.lower().endswith(('.png', '.jpg', '.jpeg')):  # Check if the image is already in a supported format
+        pdf.add_page()
+        pdf.image(img_path, x, y, w, h)
 
-pdf.output("Output.pdf","F")
+pdf.output("Output.pdf", "F")
