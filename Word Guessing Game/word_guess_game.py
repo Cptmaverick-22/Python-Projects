20c0d3f6d664c85179c118df5cb44f224127c82b
Cptmaverick-22 <sourasishain@gmail.com>
2024-09-03 22:26:12 +0530

    Updated word_guess_game

--- Word Guessing Game/word_guess_game.py
+++ Word Guessing Game/word_guess_game.py
@@ -1,28 +1,66 @@
 import random as rn
 
+def checking(letter):
+    whole_word = input("Enter the whole word: ")
+
+    if whole_word == random_choice:
+        print("Congratulations You got it !!!!")
+    else:
+        print("Ahh, you lose :( ")
+
+    print()
+    input("Please press enter to leave the program")
+
+
 words = ['hello','dog','apple','class','orange','melon','straw','game','word','elephant','good','marvel','disney']
 random_choice = rn.choice(words)
 hint = random_choice[0]+random_choice[-1]
 
+print(random_choice)
+
 name = input("Enter your name: ")
+print()
+
 print("Welcome to word game",name)
+print()
+
 print("You will have 6 chances to guess the word")
+print()
+print()
+
 chance = 6
 guessed = []
+correct_guess = {}
+counting = 0
 
 for i in range(chance):
     while True:   
-        letter = input("guess a letter: ")
+        letter = input("guess a letter or type 'quit' to guess the complete word: " )
 
         if len(letter) == 1:
             break
+        if letter == "quit":
+            checking(letter)
+
         else:
             print("please guess a letter")
 
     if letter in random_choice:
-        print("you have guessed one letter correctly")
-        guessed.append(letter)
+        count = random_choice.count(letter)
+        counting +=1
+        
 
+        if letter not in correct_guess:
+            correct_guess[letter] = 0
+            counting = 0
+        
+        if correct_guess[letter] < count: 
+            correct_guess[letter]+=1
+            guessed.append(letter)
+            print(f"You have guessed one letter correctly! '{letter}' appears {counting + 1 } Time")
+        else:
+            print("You have already guessed maximum of this letter")
+    
     else:
         print("Incorrect guess")
 
@@ -40,12 +78,4 @@ print(f"You have guessed {len(guessed)} letters correctly and the letters are: "
 print()
 print("Now guess the whole word")
 
-whole_word = input("Enter the whole word: ")
-
-if whole_word == random_choice:
-    print("Congratulations You got it !!!!")
-else:
-    print("Ahh, you lose :( ")
-
-print()
-input("Please press enter to leave the program")
+checking(letter)
